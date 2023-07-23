c30424ca524b4913d86887d3427acc6d0adeeed3
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2023-07-23 19:28:36 +0530

    New build Stuck in bug fixing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import ReactDOM, { render } from 'react-dom'
 import App  from './App'
-import './index.css'
+import './components/index.css'
 
 ReactDOM.render(<App/>, document.querySelector("#root"))
