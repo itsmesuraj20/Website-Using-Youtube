a3c749b5f157c5e55264f4162a92c87c37bbfdf6
itsmesuraj20 <rameshwarsingh99351@gmail.com>
2023-07-23 22:04:43 +0530

    Still cant find the bug

--- index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM, { render } from 'react-dom'
-import App  from './App'
-import './components/index.css'
-
-ReactDOM.render(<App/>, document.querySelector("#root"))
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom'; // Remove the unused import { render }
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.querySelector("#root"));
